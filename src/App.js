090daedca6af9d6b2ffc589b8cf559331124d710
ambroise <muhayimana21@gmail.com>
2023-01-18 14:52:46 +0200

    modified

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 
 
 function App() {
+  const name = "ambroise"
   return (
     <div className="App">
       <h1>Hello from reacts</h1>
-      <h1>hello</h1>
+      <h1>hello {name}</h1>
     </div>
     
   );
